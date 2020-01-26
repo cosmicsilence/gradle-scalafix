f66cce2fac2cfd751ecaaa96e43865cdec5d754c
Félix Miguel Sanjuán Segovia <fmsanse@gmail.com>
2020-01-26 23:25:45 +0000

    tests added for enableSemanticdb extension property

---
+++ src/test/groovy/io/cosmicsilence/scalafix/ScalafixPluginFunctionalTest.groovy
@@ -0,0 +1,83 @@
+package io.cosmicsilence.scalafix
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+
+import spock.lang.Specification
+
+class ScalafixPluginFunctionalTest extends Specification {
+
+    @Rule
+    public final TemporaryFolder testProjectDir = new TemporaryFolder();
+    private File settingsFile;
+    private File buildFile;
+
+    def setup() {
+        settingsFile = testProjectDir.newFile("settings.gradle");
+        buildFile = testProjectDir.newFile("build.gradle");
+
+        buildFile.write'''
+plugins {
+    id 'scala'
+    id 'io.cosmicsilence.scalafix'
+}
+
+repositories {
+    mavenCentral()
+}
+'''
+        settingsFile.write "rootProject.name = 'hello-world'"
+    }
+
+    def 'checkScalafixMain task runs compileScala by default'() {
+        when:
+        BuildResult buildResult = runGradleTask('scalafix', ['-m'])
+
+        then:
+        buildResult.output.contains(':compileScala SKIPPED')
+        buildResult.output.contains(':compileTestScala SKIPPED')
+        buildResult.output.contains(':scalafixMain SKIPPED')
+        buildResult.output.contains(':scalafixTest SKIPPED')
+    }
+
+    def 'checkScalafixMain task runs compileScala when enableSemanticdb is enabled in the scalafix extension'() {
+        given:
+        buildFile.append '''
+scalafix { enableSemanticdb = true }'''
+
+        when:
+        BuildResult buildResult = runGradleTask('scalafix', ['-m'])
+
+        then:
+        buildResult.output.contains(':compileScala SKIPPED')
+        buildResult.output.contains(':compileTestScala SKIPPED')
+        buildResult.output.contains(':scalafixMain SKIPPED')
+        buildResult.output.contains(':scalafixTest SKIPPED')
+    }
+
+    def 'checkScalafixMain task does not run compileScala when enableSemanticdb is disabled in the scalafix extension'() {
+        given:
+        buildFile.append '''
+scalafix { enableSemanticdb = false }'''
+
+        when:
+        BuildResult buildResult = runGradleTask('scalafix', ['-m'])
+
+        then:
+        !buildResult.output.contains(':compileScala SKIPPED')
+        !buildResult.output.contains(':compileTestScala SKIPPED')
+        buildResult.output.contains(':scalafixMain SKIPPED')
+        buildResult.output.contains(':scalafixTest SKIPPED')
+    }
+
+    BuildResult runGradleTask(String task, List<String> arguments) {
+        arguments.add(task)
+        return GradleRunner.create()
+                .withProjectDir(testProjectDir.getRoot())
+                .withArguments(arguments)
+                .withPluginClasspath()
+                .build()
+    }
+}
