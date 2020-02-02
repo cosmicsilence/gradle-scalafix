5029c1400ab31aa4920e9c5dd26692cc7591c9d8
Marcelo Cenerino <marcelocenerine@gmail.com>
2020-02-02 22:54:26 +0000

    turn scalaVersion/compileOptions/classpath/sourceRoot into task inputs + fix NPE

--- src/main/groovy/io/github/cosmicsilence/scalafix/ScalafixTask.groovy
+++ src/main/groovy/io/github/cosmicsilence/scalafix/ScalafixTask.groovy
@@ -1,23 +1,20 @@
 package io.github.cosmicsilence.scalafix
 
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.file.FileCollection
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.tasks.*
-import org.gradle.api.tasks.scala.ScalaCompile
 import scalafix.interfaces.Scalafix
 import scalafix.interfaces.ScalafixMainMode
-
-import java.nio.file.Path
 
 class ScalafixTask extends SourceTask {
 
     private static final Logger logger = Logging.getLogger(ScalafixTask)
 
     @InputFile
-    @PathSensitive(PathSensitivity.RELATIVE)
     @Optional
     final RegularFileProperty configFile = project.objects.fileProperty()
 
@@ -28,6 +25,21 @@ class ScalafixTask extends SourceTask {
     @Input
     ScalafixMainMode mode
 
+    @Input
+    @Optional
+    String scalaVersion
+
+    @Input
+    @Optional
+    List<String> compileOptions
+
+    @Input
+    @Optional
+    FileCollection classpath
+
+    @Input
+    File sourceRoot
+
     @TaskAction
     void run() {
         if (!source.isEmpty()) processSources()
@@ -36,45 +48,44 @@ class ScalafixTask extends SourceTask {
 
     private void processSources() {
         def sourcePaths = source.collect { it.toPath() }
-        def config = java.util.Optional.ofNullable(configFile.getOrNull()).map { it.asFile.toPath() }
+        def configPath = java.util.Optional.ofNullable(configFile.getOrNull()).map { it.asFile.toPath() }
+        def projectClasspath = classpath.collect { it.toPath() }
         def cliDependency = project.dependencies.create(BuildInfo.scalafixCliArtifact)
         def cliClasspath = project.configurations.detachedConfiguration(cliDependency)
         def customRulesClasspath = project.configurations.getByName(ScalafixPlugin.CUSTOM_RULES_CONFIGURATION)
-        def scalacVersion = getScalaVersion()
-        def scalacOptions = getScalacOptions()
-        def projectClasspath = getProjectClasspath()
 
         logger.debug(
-                """Initialising Scalafix with the following parameters:
+                """Running Scalafix with the following arguments:
                   | - Mode: ${mode}
                   | - Config file: ${configFile}
                   | - Custom rules classpath: ${customRulesClasspath.asPath}
-                  | - Scala version: ${scalacVersion}
-                  | - Scalac options: ${scalacOptions}
+                  | - Scala version: ${scalaVersion}
+                  | - Scalac options: ${compileOptions}
+                  | - Source root: ${sourceRoot}
                   | - Sources: ${sourcePaths}
                   | - Classpath: ${projectClasspath}
                   |""".stripMargin())
 
         def interfacesClassloader = new InterfacesClassloader(getClass().classLoader)
         def cliClassloader = classloaderFrom(cliClasspath, interfacesClassloader)
-        def toolsClassloader = classloaderFrom(customRulesClasspath, cliClassloader)
+        def customRulesClassloader = classloaderFrom(customRulesClasspath, cliClassloader)
 
         def args = Scalafix.classloadInstance(cliClassloader)
                 .newArguments()
                 .withMode(mode)
-                .withConfig(config)
+                .withConfig(configPath)
                 .withRules(rules.get())
-                .withSourceroot(project.projectDir.toPath())
+                .withSourceroot(sourceRoot.toPath())
                 .withPaths(sourcePaths)
-                .withToolClasspath(toolsClassloader)
+                .withToolClasspath(customRulesClassloader)
                 .withClasspath(projectClasspath)
-                .withScalaVersion(getScalaVersion())
-                .withScalacOptions(getScalacOptions())
+                .withScalaVersion(scalaVersion)
+                .withScalacOptions(compileOptions)
 
         logger.debug(
-                """Scalafix initialised!:
-                  | - Rules available: ${args.availableRules().collect { it.name() }}
-                  | - Rules that will run: ${args.rulesThatWillRun().collect { it.name() }}
+                """Scalafix rules:
+                  | - Available: ${args.availableRules().collect { it.name() }}
+                  | - That will run: ${args.rulesThatWillRun().collect { it.name() }}
                   |""".stripMargin())
 
         if (!args.rulesThatWillRun().empty) {
@@ -84,27 +95,6 @@ class ScalafixTask extends SourceTask {
         } else {
             logger.warn("No Scalafix rules to run")
         }
-    }
-
-    private List<String> getScalacOptions() {
-        getCompileTask().scalaCompileOptions.additionalParameters ?: []
-    }
-
-    private String getScalaVersion() {
-        def scalaRuntime = project.extensions.findByType(ScalaRuntime.class)
-        def scalaJar = scalaRuntime.findScalaJar(getCompileTask().classpath, "library")
-        scalaRuntime.getScalaVersion(scalaJar)
-    }
-
-    private ScalaCompile getCompileTask() {
-        project.tasks.withType(ScalaCompile).first()
-    }
-
-    private List<Path> getProjectClasspath() {
-        def classesDirs = project.sourceSets.collect { SourceSet ss ->
-            ss.output.classesDirs.toList()
-        }.flatten()
-        classesDirs.collect { it.toPath() }
     }
 
     private static URLClassLoader classloaderFrom(Configuration configuration, ClassLoader parent) {
